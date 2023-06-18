a3814850faeda5294d6aaabc487f1cf1294a7262
Batuhan Erden <erden.batuhan@gmail.com>
2023-06-18 17:57:55 +0200

    bbse-bank-2.0 - Foundry: Write the tests for BBSEToken

---
+++ projects/bbse-bank-2.0/runners/foundry/test/BBSEToken.t.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./extensions/ExtendedDSTest.sol";
+import "src/BBSEToken.sol";
+
+abstract contract BBSETokenTest is ExtendedDSTest {
+
+  BBSEToken bbseToken;
+
+  function setUp() public {
+    bbseToken = new BBSEToken();
+  }
+}
+
+contract BBSETokenTest_SucceedScenarios is BBSETokenTest {
+
+  function test_1_SucceedIf_TokenNameIsSetCorrectly() public {
+    assertEq(bbseToken.name(), "BBSE TOKEN", "Token name is not set correctly");
+  }
+
+  function test_2_SucceedIf_TokenSymbolIsSetCorrectly() public {
+    assertEq(bbseToken.symbol(), "BBSE", "Token symbol is not set correctly");
+  }
+
+  function test_3_SucceedIf_MinterIsSetCorrectly() public {
+    assertEq(bbseToken.minter(), address(this), "Minter is not set correctly"); // address(this) is the owner
+  }
+
+  function test_4_SucceedIf_MinterRoleIsPassedCorrectly() public {
+    bbseToken.passMinterRole(address(1));
+    assertEq(bbseToken.minter(), address(1), "Minter role is not passed correctly");
+  }
+
+  function test_5_SucceedIf_TokensAreMintedCorrectly() public {
+    bbseToken.mint(address(1), 10);
+    assertEq(bbseToken.balanceOf(address(1)), 10, "Tokens are not minted correctly");
+  }
+}
+
+contract BBSETokenTest_FailureScenarios is BBSETokenTest {
+
+  function test_1_RevertWhen_NonMinterPassesMinterRole() public {
+    vm.prank(address(1)); // Inject a change of user to a non-minter one
+
+    // Attempt to pass the minter role without being the minter and expect the call to be reverted
+    vm.expectRevert(bytes("You are not the minter"));
+    bbseToken.passMinterRole(address(2));
+  }
+
+  function test_2_RevertWhen_NonMinterMintsTokens() public {
+    vm.prank(address(1)); // Inject a change of user to a non-minter one
+
+    // Attempt to mint tokens without being the minter and expect the call to be reverted
+    vm.expectRevert(bytes("You are not the minter"));
+    bbseToken.mint(address(1), 10);
+  }
+}
---
+++ projects/bbse-bank-2.0/runners/foundry/test/extensions/ExtendedDSTest.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/console.sol";
+import "ds-test/test.sol";
+
+interface Vm {
+
+  function prank(address) external;
+  function deal(address, uint256) external;
+  function roll(uint256) external;
+  function expectRevert() external;
+  function expectRevert(bytes calldata) external;
+  function expectEmit() external;
+}
+
+abstract contract ExtendedDSTest is DSTest {
+
+  Vm vm = Vm(HEVM_ADDRESS); // HEVM_ADDRESS is 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
+
+  constructor() {}
+}
