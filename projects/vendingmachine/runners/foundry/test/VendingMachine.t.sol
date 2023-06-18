ce1fce86b24667190187bc0a4e17cb12a66597b9
Batuhan Erden <erden.batuhan@gmail.com>
2023-06-18 15:52:02 +0200

    vendingmachine - Foundry: update tests

--- projects/vendingmachine/runners/foundry/test/VendingMachine.t.sol
+++ projects/vendingmachine/runners/foundry/test/VendingMachine.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.11;
 
 import "forge-std/console.sol";
 import "ds-test/test.sol";
-import "src/VendingMachine.sol";
+import "../src/VendingMachine.sol";
 
 interface Vm {
 
@@ -14,18 +14,18 @@ interface Vm {
 
 contract TestVendingMachine is DSTest {
 
-  Vm private vm = Vm(HEVM_ADDRESS); // HEVM_ADDRESS is 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D)
-  VendingMachine private vendingMachine;
-
   uint256 private constant INITIAL_BALANCE = 100;
   uint256 private constant DONUT_PRICE_IN_ETHERS = 2;
+
+  Vm private vm = Vm(HEVM_ADDRESS); // HEVM_ADDRESS is 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
+  VendingMachine private vendingMachine;
 
   function setUp() public {
     vendingMachine = new VendingMachine();
   }
 
   // Ensures that the starting balance of the vending machine is the initial balance
-  function testInitialBalance() public {
+  function test_1_SuccessIf_InitialBalanceIsAsExpected() public {
     uint256 actualBalance = vendingMachine.getVendingMachineBalance();
     uint256 expectedBalance = INITIAL_BALANCE;
 
@@ -33,7 +33,7 @@ contract TestVendingMachine is DSTest {
   }
 
   // Ensures the balance of the vending machine can be updated
-  function testBalanceUpdate() public {
+  function test_2_SuccessIf_BalanceUpdateSucceeds() public {
     uint256 amountToBeRestocked = 100;
 
     vendingMachine.restock(amountToBeRestocked);
@@ -45,7 +45,7 @@ contract TestVendingMachine is DSTest {
   }
 
   // Allows donuts to be purchased
-  function testDonutPurchase() public {
+  function test_3_SuccessIf_DonutsCanBePurchased() public {
     uint256 amountToBePurchased = 40;
     uint256 purchaseCost = amountToBePurchased * DONUT_PRICE_IN_ETHERS * 1 ether;
 
@@ -59,7 +59,7 @@ contract TestVendingMachine is DSTest {
   }
 
   // Allows multiple accounts to purchase donuts
-  function testMultipleAccountPurchase() public {
+  function test_4_SuccessIf_MultipleAccountsCanPurchase() public {
     uint256 numBuyers = 4;
     uint256 amountToBePurchased = 10;
     uint256 purchaseCost = amountToBePurchased * DONUT_PRICE_IN_ETHERS * 1 ether;
@@ -93,7 +93,7 @@ contract TestVendingMachine is DSTest {
   }
 
   // Prevents purchasing more donuts than available in the vending machine
-  function testInsufficientDonuts() public {
+  function test_5_RevertWhen_InsufficientDonutsAvailable() public {
     uint256 amountToBePurchased = INITIAL_BALANCE + 1; // More than available donuts
     uint256 purchaseCost = amountToBePurchased * DONUT_PRICE_IN_ETHERS * 1 ether;
 
@@ -103,21 +103,21 @@ contract TestVendingMachine is DSTest {
   }
 
   // Prevents purchasing donuts without providing sufficient payment
-  function testInsufficientPayment() public {
+  function test_6_RevertWhen_InsufficientPaymentProvided() public {
     uint256 amountToBePurchased = 30;
     uint256 purchaseCost = amountToBePurchased * DONUT_PRICE_IN_ETHERS * 1 ether - 1; // Insufficient payment
 
-    // Attempt to purchase without providing sufficient payment
+    // Attempt to purchase without providing sufficient payment and expect the call to be reverted
     vm.expectRevert(bytes("You must pay at least 2 ETH per donut"));
     vendingMachine.purchase{value: purchaseCost}(amountToBePurchased);
   }
 
   // Prevents non-owner addresses from restocking the vending machine
-  function testNonOwnerRestocking() public {
+  function test_7_RevertWhen_NonOwnerRestocks() public {
     // inject a change of user to a non-owner one, which is not address(this)
     vm.prank(address(5));
 
-    // Attempt to restock from a non-owner address
+    // Attempt to restock from a non-owner address and expect the call to be reverted
     vm.expectRevert(bytes("Only the owner can restock"));
     vendingMachine.restock(10);
   }
