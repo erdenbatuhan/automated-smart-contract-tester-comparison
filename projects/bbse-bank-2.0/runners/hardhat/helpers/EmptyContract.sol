e430e1002b9c0ba92c90afd656029dc282eecdef
Batuhan Erden <erden.batuhan@gmail.com>
2023-07-14 06:05:54 +0200

    optimize the hardhat dockerization as well

---
+++ projects/bbse-bank-2.0/runners/hardhat/helpers/EmptyContract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Libraries expected to be used
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Empty Contract
+contract EmptyContract {}
---
+++ projects/vendingmachine/runners/hardhat/helpers/EmptyContract.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Libraries expected to be used
+
+// Empty Contract
+contract EmptyContract {}
