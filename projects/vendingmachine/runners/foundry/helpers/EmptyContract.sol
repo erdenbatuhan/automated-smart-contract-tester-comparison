ac101c2aa34d4aef6f3da5e003f11c7ad0bba92c
Batuhan Erden <erden.batuhan@gmail.com>
2023-07-14 05:54:46 +0200

    fix issue in vendingmachine-foundry project

--- projects/vendingmachine/runners/foundry/helpers/EmptyContract.sol
+++ projects/vendingmachine/runners/foundry/helpers/EmptyContract.sol
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.11;
 
 // Libraries expected to be used
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "forge-std/console.sol";
 
 // Empty Contract
 contract EmptyContract {}
